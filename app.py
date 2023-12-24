ae5550e8dadf7432087653d223816f2a7b1a2f3b
suzuyu0115 <szkyut0115@gmail.com>
2023-12-24 18:23:34 +0900

    Add: 会議室予約画面の作成1(ユーザー、会議室一覧取得)

--- app.py
+++ app.py
@@ -29,70 +29,91 @@ if page == 'users':
     st.json(res.json())
 
 elif page == 'rooms':
-    st.title('会議室登録画面')
+  st.title('会議室登録画面')
 
-    with st.form(key='room'):
-      # room_id: int = random.randint(0, 10)
-      room_name: str = st.text_input('会議室名', max_chars = 12)
-      capacity: int = st.number_input('定員', step=1)
-      data = {
-        # 'room_id': room_id,
-        'room_name': room_name,
-        'capacity': capacity
-      }
-      submit_button = st.form_submit_button(label='会議室登録')
+  with st.form(key='room'):
+    # room_id: int = random.randint(0, 10)
+    room_name: str = st.text_input('会議室名', max_chars = 12)
+    capacity: int = st.number_input('定員', step=1)
+    data = {
+      # 'room_id': room_id,
+      'room_name': room_name,
+      'capacity': capacity
+    }
+    submit_button = st.form_submit_button(label='会議室登録')
 
-    if submit_button:
-      url = 'http://127.0.0.1:8000/rooms'
-      res = requests.post(
-        url,
-        data=json.dumps(data)
-      )
-      if res.status_code == 200:
-        st.success('会議室登録完了')
-      st.json(res.json())
+  if submit_button:
+    url = 'http://127.0.0.1:8000/rooms'
+    res = requests.post(
+      url,
+      data=json.dumps(data)
+    )
+    if res.status_code == 200:
+      st.success('会議室登録完了')
+    st.json(res.json())
 
 elif page == 'bookings':
-    st.title('APIテスト画面(予約)')
+  st.title('会議室予約画面')
+  # ユーザー一覧取得
+  url_users = 'http://127.0.0.1:8000/users'
+  res = requests.get(url_users)
+  users = res.json()
+  # ユーザー名をキー、ユーザーIDをバリュー
+  users_dict = {}
+  for user in users:
+    users_dict[user['username']] = user['user_id']
 
-    with st.form(key='bookings'):
-      booking_id: int = random.randint(0, 10)
-      user_id: int = random.randint(0, 10)
-      room_id: int = random.randint(0, 10)
-      booked_num: int = st.number_input('予約人数', step=1)
-      date = st.date_input('日付： ', min_value=datetime.date.today())
-      start_time = st.time_input('開始時刻： ', value=datetime.time(hour=9, minute=0))
-      end_time = st.time_input('終了時刻： ', value=datetime.time(hour=20, minute=0))
-      data = {
-        'booking_id': booking_id,
-        'user_id': user_id,
-        'room_id': room_id,
-        'booked_num': booked_num,
-        'start_date_time': datetime.datetime(
-          year=date.year,
-          month=date.month,
-          day=date.day,
-          hour=start_time.hour,
-          minute=start_time.minute
-        ).isoformat(),
-        'end_date_time': datetime.datetime(
-          year=date.year,
-          month=date.month,
-          day=date.day,
-          hour=end_time.hour,
-          minute=end_time.minute
-        ).isoformat()
-      }
-      submit_button = st.form_submit_button(label='リクエスト送信')
+  # 会議室一覧の取得
+  url_rooms = 'http://127.0.0.1:8000/rooms'
+  res = requests.get(url_rooms)
+  rooms = res.json()
+  st.json(rooms)
+  rooms_dict = {}
+  for room in rooms:
+    rooms_dict[room['room_name']] = {
+      'room_id': room['room_id'],
+      'capacity': room['capacity']
+    }
+  st.write(rooms_dict)
 
-    if submit_button:
-      st.write('## 送信データ')
-      st.json(data)
-      st.write('## レスポンス結果')
-      url = 'http://127.0.0.1:8000/bookings'
-      res = requests.post(
-        url,
-        data=json.dumps(data)
-      )
-      st.write(res.status_code)
-      st.json(res.json())
+  # with st.form(key='bookings'):
+  #   booking_id: int = random.randint(0, 10)
+  #   user_id: int = random.randint(0, 10)
+  #   room_id: int = random.randint(0, 10)
+  #   booked_num: int = st.number_input('予約人数', step=1)
+  #   date = st.date_input('日付： ', min_value=datetime.date.today())
+  #   start_time = st.time_input('開始時刻： ', value=datetime.time(hour=9, minute=0))
+  #   end_time = st.time_input('終了時刻： ', value=datetime.time(hour=20, minute=0))
+  #   data = {
+  #     'booking_id': booking_id,
+  #     'user_id': user_id,
+  #     'room_id': room_id,
+  #     'booked_num': booked_num,
+  #     'start_date_time': datetime.datetime(
+  #       year=date.year,
+  #       month=date.month,
+  #       day=date.day,
+  #       hour=start_time.hour,
+  #       minute=start_time.minute
+  #     ).isoformat(),
+  #     'end_date_time': datetime.datetime(
+  #       year=date.year,
+  #       month=date.month,
+  #       day=date.day,
+  #       hour=end_time.hour,
+  #       minute=end_time.minute
+  #     ).isoformat()
+  #   }
+  #   submit_button = st.form_submit_button(label='リクエスト送信')
+
+  # if submit_button:
+  #   st.write('## 送信データ')
+  #   st.json(data)
+  #   st.write('## レスポンス結果')
+  #   url = 'http://127.0.0.1:8000/bookings'
+  #   res = requests.post(
+  #     url,
+  #     data=json.dumps(data)
+  #   )
+  #   st.write(res.status_code)
+  #   st.json(res.json())
