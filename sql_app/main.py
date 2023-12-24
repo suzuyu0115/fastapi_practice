f9cb983b8ae9c25956ecaadd28617b5d2c8e54fd
suzuyu0115 <szkyut0115@gmail.com>
2023-12-24 17:12:56 +0900

    Add: ユーザー登録画面の作成

--- sql_app/main.py
+++ sql_app/main.py
@@ -1,7 +1,6 @@
 from typing import List
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
-from pydantic import BaseModel, Field
 
 from . import crud, models, schemas
 from .database import SessionLocal, engine
@@ -41,13 +40,13 @@ async def read_bookings(skip: int = 0, limit: int = 100, db: Session = Depends(g
 
 # Create
 @app.post("/users", response_model=schemas.User)
-async def create_user(user: schemas.User, db: Session = Depends(get_db)):
+async def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
   return crud.create_user(db=db, user=user)
 
 @app.post("/rooms", response_model=schemas.Room)
-async def create_room(room: schemas.Room, db: Session = Depends(get_db)):
+async def create_room(room: schemas.RoomCreate, db: Session = Depends(get_db)):
   return crud.create_room(db=db, room=room)
 
 @app.post("/bookings", response_model=schemas.Booking)
-async def create_booking(booking: schemas.Booking, db: Session = Depends(get_db)):
+async def create_booking(booking: schemas.BookingCreate, db: Session = Depends(get_db)):
   return crud.create_booking(db=db, booking=booking)
